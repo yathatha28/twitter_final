0e0843ad6da15eb40ffe0eb462c0691fdea072f9
ameenmanna8824 <52666434+ameenmanna8824@users.noreply.github.com>
2020-09-24 17:36:23 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -31,8 +31,8 @@ api = create_api()
 
 while True:
     user = api.get_user('the_ameen_manna')
-    api.update_profile(name=f'AMEEN|{follower_count(user)} Followers')
-    print(f'Updating Twitter Name : AMEEN|{follower_count(user)} Followers')
+    api.update_profile(name=f'ameen|{follower_count(user)} Followers')
+    print(f'Updating Twitter Name : ameen|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
   
