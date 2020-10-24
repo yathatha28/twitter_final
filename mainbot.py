2ce10d20c4c91dd89aede565b2d5ef1e99e19777
yathatha28 <73296462+yathatha28@users.noreply.github.com>
2020-10-24 19:23:18 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -30,9 +30,9 @@ def follower_count(user):
 api = create_api()
 
 while True:
-    user = api.get_user('the_ameen_manna')
-    api.update_profile(name=f'ameen|{follower_count(user)} Followers')
-    print(f'Updating Twitter Name : ameen|{follower_count(user)} Followers')
+    user = api.get_user('yathartha0282')
+    api.update_profile(name=f'yathartha|{follower_count(user)} Followers')
+    print(f'Updating Twitter Name : yathartha|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
   
